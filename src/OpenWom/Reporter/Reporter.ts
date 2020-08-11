ee27836c0a5f19bed760d6dfd4703b325090af89
DomingoPintoTorres <dpt001@alumnos.ucn.cl>
2020-08-11 18:11:05 -0400

    Network History: method 'getNetworkHistory' in Reporter.ts return all data as 'Array<object>'

--- src/OpenWom/Reporter/Reporter.ts
+++ src/OpenWom/Reporter/Reporter.ts
@@ -7,6 +7,7 @@ export class Reporter {
   }
 
   public addHistory(newHistory: Array<object>): void {
+    // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < newHistory.length; i++) {
       this.networkHistory.push(newHistory[i]);
     }
@@ -14,10 +15,15 @@ export class Reporter {
 
   public consoleLogNetwork(): void {
     console.log(Object.keys(this.networkHistory[0]));
+    // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < this.networkHistory.length; i++) {
       const str = Object.values(this.networkHistory[i]);
       console.log(str);
     }
+  }
+
+  public getNetworkHistory(): Array<object> {
+    return this.networkHistory;
   }
 
 }
